917364a49c2eb5646cbfb4dc2f4f047c6e7c43d4
shaokat <shaokatcse@gmail.com>
2020-06-25 16:15:20 +0200

    api-doc file added

---
+++ api.doc.js
@@ -0,0 +1,36 @@
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+
+// Extended: https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      version: "1.0.0",
+      title: "Hotel API",
+      description: "Hotel API Information",
+      contact: {
+        name: "Shaokat Hossain",
+        email: "shaokatcse@gmail.com"
+      },
+      servers: ["https://mean-hotel-api.herokuapp.com"]
+    }
+  },
+  // ['.routes/*.js']
+  apis: ["api.doc.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+// Routes
+/**
+ * @swagger
+ * /api/hotels:
+ *  get:
+ *    description: Use to request all hotels
+ *    responses:
+ *      '200':
+ *        description: A successful response
+ */
+
+module.exports = swaggerDocs
